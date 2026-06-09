816131d258336ae0f0aac5c1dfdcdd25ff79f2db
Bohdan <yehorovbohdan.work@gmail.com>
2026-06-09 23:53:02 +0300

    Redesign Array container

--- Include/Volt/ADT/Array.h
+++ Include/Volt/ADT/Array.h
@@ -30,31 +30,71 @@ namespace Volt
 
 	public:
 		Array() = default;
-		Array(const std::initializer_list<T>& List);
-		Array(SizeType Len, const T& Value);
+		Array(SizeType Len, const ValueType& Fill = ValueType()) { Resize(Len, Fill); }
+		Array(const std::initializer_list<ValueType>& List)
+		{
+			static_assert(std::is_copy_constructible_v<ValueType>);
+			RawResize(List.size());
+			CopyRange(Data, List.begin(), Len);
+		}
 
-		Array(const Array& Other);
-		Array(Array&& Other) noexcept;
+		Array(const Array& Other)
+		{
+			static_assert(std::is_copy_constructible_v<ValueType>);
+			Len = Other.Len;
+			Cap = Other.Cap;
+			Data = Alloc.Allocate(Cap);
+			CopyRange(Data, Other.Data, Len);
+		}
 
-		Array& operator=(const Array& Other);
-		Array& operator=(Array&& Other) noexcept;
+		Array(Array&& Other) noexcept
+		{
+			std::swap(Len, Other.Len);
+			std::swap(Cap, Other.Cap);
+			std::swap(Data, Other.Data);
+		}
 
 		~Array() { Deallocate(); }
 
-		void Reserve(SizeType NewCap);
-		void Resize(SizeType NewLen, const ValueType& FillValue);
+		std::enable_if_t<std::is_copy_constructible_v<ValueType>, Array&>
+		operator=(const Array& Other) noexcept
+		{
+			if (this != &Other)
+			{
+				ValueType* NewData = Alloc.Allocate(Other.Cap);
+				CopyRange(NewData, Other.Data, Other.Len);
+				if (Data) Deallocate();
+				Data = NewData;
+				Len = Other.Len;
+				Cap = Other.Cap;
+			}
 
-		void Add(const T& Value);
-		void Pop();
+			return *this;
+		}
 
-		template <typename ... Args_>
-		void Emplace(Args_ ... Args);
+		Array& operator=(Array&& Other) noexcept
+		{
+			if (this != &Other)
+			{
+				if (Data)
+					Deallocate();
+				Len = Other.Len;
+				Cap = Other.Cap;
+				Data = Other.Data;
+				Other.Reset();
+			}
 
-		[[nodiscard]] const T& operator[](SizeType Index) const { return Data[Index]; }
-		[[nodiscard]] T& operator[](SizeType Index) { return Data[Index]; }
+			return *this;
+		}
+
+		[[nodiscard]] ValueType& operator[](SizeType Index) { return Data[Index]; }
+		[[nodiscard]] const ValueType& operator[](SizeType Index) const { return Data[Index]; }
 
 		[[nodiscard]] SizeType Length() const { return Len; }
 		[[nodiscard]] SizeType Capacity() const { return Cap; }
+		[[nodiscard]] ValueType* RawData() { return Data; }
+		[[nodiscard]] const ValueType* RawData() const { return Data; }
+
 		[[nodiscard]] bool Empty() const { return Len == 0; }
 
 		[[nodiscard]] ValueType& Front() { return Data[0]; }
@@ -72,205 +112,179 @@ namespace Volt
 		[[nodiscard]] ConstIterator begin() const { return ConstIterator(Data); }
 		[[nodiscard]] ConstIterator end()   const { return ConstIterator(Data + Len); }
 
-	private:
-		void RawResize(SizeType NewLen);
-		void Deallocate();
-
-		static SizeType CalculateCapacity(SizeType InSize) { return InSize * 2; }
-	};
-
-	template<typename T, typename Alloca>
-	Array<T, Alloca>::Array(const std::initializer_list<T> &List)
-		: Len(List.size()), Cap(CalculateCapacity(Len))
-	{
-		Data = Alloc.Allocate(Cap);
-		SizeType i = 0;
-
-		for (const T& El : List)
+		void Reserve(SizeType NewCap)
 		{
-			Alloc.Construct(Data + i, El);
-			i++;
-		}
-	}
-
-	template<typename T, typename Alloca>
-	Array<T, Alloca>::Array(SizeType Len, const T &Value)
-		: Len(Len), Cap(CalculateCapacity(Len))
-	{
-		Data = Alloc.Allocate(Cap);
-
-		for (SizeType i = 0; i < Len; i++)
-			Alloc.Construct(Data + i, Value);
-	}
-
-	template<typename T, typename Alloca>
-	Array<T, Alloca>::Array(const Array &Other)
-	{
-		if (Other.Len == 0)
-			return;
-
-		Data = Alloc.Allocate(Other.Cap);
-		for (SizeType i = 0; i < Other.Len; i++)
-			Alloc.Construct(Data + i, Other.Data[i]);
-
-		Len = Other.Len;
-		Cap = Other.Cap;
-	}
-
-	template<typename T, typename Alloca>
-	Array<T, Alloca>::Array(Array &&Other) noexcept
-	{
-		Data = Other.Data;
-		Len = Other.Len;
-		Cap = Other.Cap;
-
-		Other.Data = nullptr;
-		Other.Len = 0;
-		Other.Cap = 0;
-	}
-
-	template<typename T, typename Alloca>
-	Array<T, Alloca> &Array<T, Alloca>::operator=(const Array &Other)
-	{
-		if (this != &Other)
-		{
-			Deallocate();
-
-			Data = Alloc.Allocate(Other.Cap);
-			for (SizeType i = 0; i < Other.Len; i++)
-				Alloc.Construct(Data + i, Other.Data[i]);
-
-			Cap = Other.Cap;
-			Len = Other.Len;
-		}
-
-		return *this;
-	}
-
-	template<typename T, typename Alloca>
-	Array<T, Alloca> &Array<T, Alloca>::operator=(Array &&Other) noexcept
-	{
-		if (this != &Other)
-		{
+			if (NewCap <= Cap) return;
+			ValueType* NewData = Alloc.Allocate(NewCap);
 			if (Data)
+			{
+				TransferRange(NewData, Data, Len);
+				DestructRange(Data, Len);
+				Alloc.Deallocate(Data);
+			}
+			Data = NewData;
+			Cap = NewCap;
+		}
+
+		void Add(const ValueType& Value)
+		{
+			if (Len >= Cap) Reserve(CalculateCapacity(Cap));
+			Copy(Data + Len, Value);
+			Len++;
+		}
+
+		void Add(ValueType&& Value)
+		{
+			if (Len >= Cap) Reserve(CalculateCapacity(Cap));
+			Move(Data + Len, std::move(Value));
+			Len++;
+		}
+
+		template <typename ...Args_>
+		void Emplace(Args_&&... Args)
+		{
+			if (Len >= Cap) Reserve(CalculateCapacity(Cap));
+			Alloc.Construct(Data + Len, std::forward<Args_>(Args)...);
+			Len++;
+		}
+
+		void Pop()
+		{
+			if (Len == 0) return;
+			Destruct(Data + (Len - 1));
+			Len--;
+		}
+
+		void Resize(SizeType NewLen, const ValueType& Fill = ValueType())
+		{
+			SizeType OldLen = Len;
+			RawResize(NewLen);
+			if (Len > OldLen)
+				for (SizeType i = OldLen; i < Len; i++)
+					Copy(Data + i, Fill);
+		}
+
+		void ShrinkToFit()
+		{
+			if (Len == Cap) return;
+			if (Len == 0)
+			{
 				Deallocate();
+				return;
+			}
 
-			Data = Other.Data;
-			Cap = Other.Cap;
-			Len = Other.Len;
-
-			Other.Data = nullptr;
-			Other.Cap = 0;
-			Other.Len = 0;
+			ValueType* NewData = Alloc.Allocate(Len);
+			TransferRange(NewData, Data, Len);
+			DestructRange(Data, Len);
+			Alloc.Deallocate(Data);
+			Data = NewData;
+			Cap = Len;
 		}
 
-		return *this;
-	}
-
-	template<typename T, typename Alloca>
-	void Array<T, Alloca>::Reserve(SizeType NewCap)
-	{
-		if (NewCap <= Cap) return;
-
-		ValueType* NewData = Alloc.Allocate(NewCap);
-		for (SizeType i = 0; i < Len; i++)
+	private:
+		static SizeType CalculateCapacity(SizeType InCap)
 		{
-			Alloc.Construct(NewData + i, std::move(Data[i]));
-			Alloc.Destruct(Data + i);
+			if (InCap < 32) return 32;
+			return SizeType(1) << (static_cast<SizeType>(std::bit_width(InCap)));
 		}
 
-		Alloc.Deallocate(Data);
-		Data = NewData;
-		Cap = NewCap;
-	}
-
-	template<typename T, typename Alloca>
-	void Array<T, Alloca>::Resize(SizeType NewLen, const ValueType &FillValue)
-	{
-		if (NewLen == Len) return;
-
-		if (NewLen < Len)
+		void RawResize(SizeType NewLen)
 		{
-			for (SizeType i = NewLen; i < Len; i++)
-				Alloc.Destruct(Data + i);
-
+			if (Len == NewLen) return;
+			if (Len > NewLen)
+				DestructRange(Data + NewLen, Len - NewLen);
+			else if (Cap < NewLen)
+				Reserve(CalculateCapacity(NewLen));
 			Len = NewLen;
-			return;
 		}
 
-		if (NewLen > Cap)
-			Reserve(CalculateCapacity(NewLen));
-
-		for (SizeType i = Len; i < NewLen; i++)
-			Alloc.Construct(Data + i, FillValue);
-
-		Len = NewLen;
-	}
-
-	template<typename T, typename Alloca>
-	void Array<T, Alloca>::Add(const T &Value)
-	{
-		if (Len + 1 > Cap)
-			Reserve(CalculateCapacity(Len + 1));
-
-		Alloc.Construct(Data + Len, Value);
-		Len++;
-	}
-
-	template<typename T, typename Alloca>
-	void Array<T, Alloca>::Pop()
-	{
-		if (Len == 0) return;
-
-		Alloc.Destruct(Data + (Len - 1));
-		Len--;
-	}
-
-	template<typename T, typename Alloca>
-	template<typename ... Args_>
-	void Array<T, Alloca>::Emplace(Args_... Args)
-	{
-		if (Len + 1 > Cap)
-			Reserve(CalculateCapacity(Len + 1));
-
-		Alloc.Construct(Data + Len, std::forward<Args_>(Args)...);
-		Len++;
-	}
-
-	template<typename T, typename Alloca>
-	void Array<T, Alloca>::RawResize(SizeType NewLen)
-	{
-		if (NewLen == Len) return;
-
-		if (NewLen < Len)
+		void Reset()
 		{
-			for (SizeType i = NewLen; i < Len; i++)
-				Alloc.Destruct(Data + i);
-
-			Len = NewLen;
-			return;
+			Len = Cap = 0;
+			Data = nullptr;
 		}
 
-		if (NewLen > Cap)
-			Reserve(CalculateCapacity(NewLen));
+		void Deallocate()
+		{
+			if (!Data) return;
 
-		Len = NewLen;
-	}
+			DestructRange(Data, Len);
+			Alloc.Deallocate(Data);
+			Reset();
+		}
 
-	template<typename T, typename Alloca>
-	void Array<T, Alloca>::Deallocate()
-	{
-		if (!Data) return;
+		void Copy(ValueType* Dst, const ValueType& Src)
+		{
+			if constexpr (std::is_trivially_copyable_v<ValueType>)
+			{
+				*Dst = Src;
+				return;
+			}
+			Alloc.Construct(Dst, Src);
+		}
 
-		for (SizeType i = 0; i < Len; i++)
-			Alloc.Destruct(Data + i);
+		void Move(ValueType* Dst, ValueType&& Src)
+		{
+			if constexpr (std::is_trivially_copyable_v<ValueType>)
+			{
+				*Dst = std::move(Src);
+				return;
+			}
+			Alloc.Construct(Dst, std::move(Src));
+		}
 
-		Alloc.Deallocate(Data);
+		void Transfer(ValueType* Dst, ValueType& Src)
+		{
+			if constexpr (std::is_trivially_copyable_v<ValueType>)
+				std::memcpy(Dst, &Src, sizeof(ValueType));
+			else if constexpr (std::is_nothrow_move_constructible_v<ValueType>)
+				Alloc.Construct(Dst, std::move(Src));
+			else
+				Alloc.Construct(Dst, Src);
+		}
 
-		Data = nullptr;
-		Len = 0;
-		Cap = 0;
-	}
+		void Destruct(ValueType* Ptr)
+		{
+			if constexpr (std::is_trivially_destructible_v<ValueType>) return;
+			Alloc.Destruct(Ptr);
+		}
+
+		void CopyRange(ValueType* Dst, const ValueType* Src, SizeType Len)
+		{
+			if (Len == 0) return;
+			if constexpr (std::is_trivially_copyable_v<ValueType>)
+			{
+				std::memcpy(Dst, Src, Len * sizeof(ValueType));
+				return;
+			}
+
+			for (SizeType i = 0; i < Len; i++)
+				Alloc.Construct(Dst + i, Src[i]);
+		}
+
+		void TransferRange(ValueType* Dst, ValueType* Src, SizeType Len)
+		{
+			if (Len == 0) return;
+			if constexpr (std::is_trivially_copyable_v<ValueType>)
+				std::memcpy(Dst, Src, Len * sizeof(ValueType));
+			else if constexpr (std::is_nothrow_move_constructible_v<ValueType>)
+				for (SizeType i = 0; i < Len; i++)
+					Alloc.Construct(Dst + i, std::move(Src[i]));
+			else
+				for (SizeType i = 0; i < Len; i++)
+					Alloc.Construct(Dst + i, Src[i]);
+		}
+
+		void DestructRange(ValueType* Ptr, SizeType Len)
+		{
+			if constexpr (std::is_trivially_destructible_v<ValueType>) return;
+
+			while (Len > 0)
+			{
+				--Len;
+				Alloc.Destruct(Ptr + Len);
+			}
+		}
+	};
 }
-
 #endif //CVOLT_ARRAY_H
