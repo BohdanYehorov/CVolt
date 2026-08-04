e48e938b71b7ab55bb6a787f391e3f2ef783e630
Bohdan <yehorovbohdan.work@gmail.com>
2026-08-04 19:28:50 +0300

    Add FuncOverloadTable

---
+++ Include/Volt/Core/Functions/FuncOverloadTable.h
@@ -0,0 +1,36 @@
+//
+// Created by bohdan on 8/4/26.
+//
+
+#ifndef CVOLT_FUNCOVERLOADTABLE_H
+#define CVOLT_FUNCOVERLOADTABLE_H
+
+#include "Volt/Core/TypeDefs/FunctionDefs.h"
+
+namespace Volt
+{
+    class FuncOverloadTable
+    {
+    public:
+        using Iterator = FuncOverloadVector::iterator;
+        using ConstIterator = FuncOverloadVector::const_iterator;
+
+    private:
+        FuncOverloadVector Overloads;
+
+    public:
+        void AddOverload(ArgsVector<QualType> Args, CalleeBase* Callee)
+        {
+            Overloads.emplace_back(std::move(Args), Callee);
+        }
+
+        [[nodiscard]] const FunctionOverload* FindBestOverload(llvm::ArrayRef<QualType> Args) const;
+
+        [[nodiscard]] Iterator begin() { return Overloads.begin(); }
+        [[nodiscard]] Iterator end() { return Overloads.end(); }
+        [[nodiscard]] ConstIterator begin() const { return Overloads.begin(); }
+        [[nodiscard]] ConstIterator end() const { return Overloads.end(); }
+    };
+}
+
+#endif //CVOLT_FUNCOVERLOADTABLE_H
